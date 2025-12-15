783d1eacbe3832d23439cbdeebcc218c107f5749
David Ramiro <david@kore.cc>
2025-12-15 23:41:26 +0100

    feat(wip): add esp32 support

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
+#include <Arduino.h>
 #include "main.h"
+#include "USB.h"
+#include "USBHIDMouse.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <esp_adc_cal.h>
+#include <esp32-hal-adc.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
 /// @brief Analog pin of the photodiode
-const int photoDiodePin = A1;
+const int photoDiodePin = 1;
 /// @brief Percentage threshold of brightness change that indicates screen movement
 const float brightnessChangeThreshold = 0.2;
 
@@ -15,8 +22,8 @@ String topScreenBuf = "";
 String lowerScreenBuf = "";
 char buffer[40];
 
-Chrono chrono;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+USBHIDMouse Mouse;
 
 /// @brief Inits serial, analog pin and mouse HID
 void setup()
@@ -25,8 +32,13 @@ void setup()
   Serial.println("init");
   initScreen();
 
-  pinMode(photoDiodePin, INPUT);
+  analogReadResolution(12);
+  pinMode(photoDiodePin, INPUT);  //GPIO4
+  adcAttachPin(photoDiodePin);    //GPIO4
+
+  // initialize control over the keyboard:
   Mouse.begin();
+  USB.begin();
   countdown(5);
 }
 
@@ -40,19 +52,20 @@ void loop()
   delay(500);
 
   // get reference brightness
-  int brightness = analogRead(photoDiodePin);
+  int brightness = adc1_get_raw(ADC1_CHANNEL_0);
   sprintf(buffer, "baseline: %d", brightness);
   printBufferToScreen(true);
 
   delay(500);
 
+  // TODO: investigate mouse lag
   // reset timer, click mouse
   uint32_t start = micros();
   Mouse.click(MOUSE_LEFT);
 
   while (true)
   {
-    int delta = analogRead(photoDiodePin) - brightness;
+    int delta = adc1_get_raw(ADC1_CHANNEL_0) - brightness;
 
     // loop until brightness delta is bigger than threshold
     if (abs(delta) > (brightness * brightnessChangeThreshold))
@@ -103,14 +116,16 @@ void loop()
   }
 }
 
+#define OLED_I2C_SDA_PIN 42
+#define OLED_I2C_SCL_PIN 41
+
+#define OLED_RESET_PIN -1
+#define OLED_I2C_ADDRESS 0x3C  // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32 ??
+
 void initScreen()
 {
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
+  Wire.begin(OLED_I2C_SDA_PIN, OLED_I2C_SCL_PIN);
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS);
 
   display.clearDisplay();
   display.setTextSize(1);
--- src/main.h
+++ src/main.h
@@ -1,12 +1,4 @@
-#include <Arduino.h>
-#include "Mouse.h"
-#include "Keyboard.h"
-#include <Chrono.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
 void initScreen();
 void countdown(int seconds);
 void moveMouseVertically(bool direction);
-void printBufferToScreen(boolean segment, int glyph = 0);
+void printBufferToScreen(bool segment, int glyph = 0);
