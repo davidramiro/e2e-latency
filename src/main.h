601d88506cf7f24b2d7493ba1dabdd9385fc2b4c
David Ramiro <david@kore.cc>
2025-12-15 20:37:52 +0100

    refactor: use micros instead of chrono for tighter timing, click instead of move

--- src/main.h
+++ src/main.h
@@ -1,13 +1,10 @@
 #include <Arduino.h>
 #include "Mouse.h"
-#include <Chrono.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-void reset();
 void initScreen();
 void countdown(int seconds);
 void moveMouseVertically(bool direction);
-void printBufferToScreen(boolean segment);
-void printBufferToScreen(boolean segment, int glyph);
+void printBufferToScreen(boolean segment, int glyph = 0);
